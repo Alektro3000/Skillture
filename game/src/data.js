e4cc17cd5c73d40b2459443a9035b8110c57eaac
Alektro3000 <andgu_ba@mail.ru>
2025-11-20 21:06:47 +0300

    update Assests

--- game/src/data.js
+++ game/src/data.js
@@ -2,9 +2,9 @@ furnitureList = [
     {
         id: "Bath_Tub",
         size: {
-            x: 14,
-            y: 8,
-            z: 5
+            x: 13.999998569488525,
+            y: 7.999999523162842,
+            z: 4.5
         },
         type: "ground", 
         name: "Ванна"
@@ -12,9 +12,9 @@ furnitureList = [
     {
         id: "Bench",
         size: {
-            x: 10,
-            y: 5,
-            z: 7
+            x: 9.967382550239563,
+            y: 4.250001907348633,
+            z: 5.662097191810608
         },
         gridOffset: {
             x: 0,
@@ -27,9 +27,9 @@ furnitureList = [
     {
         id: "Couch_1A",
         size: {
-            x: 9,
-            y: 5,
-            z: 6
+            x: 9.269448518753052,
+            y: 5.101096034049988, 
+            z: 4.568237364292145
         },
         gridOffset: {
             x: 0.5,
@@ -44,7 +44,7 @@ furnitureList = [
         size: {
             x: 10,
             y: 10,
-            z: 12.1
+            z: 10
         },
         type: "ground",
         name: "Куб"
@@ -68,6 +68,16 @@ furnitureList = [
         },
         type: "back-wall",
         name: "Зеркало"
+    },
+    {
+        id: "Kitchen_Cabinet_1A",
+        size: {
+            x: 3.8962554931640625,
+            y: 4.2823028564453125, 
+            z: 6.677371263504028
+        },
+        type: "back-wall",
+        name: "Шкафчик"
     }
 
 ]
